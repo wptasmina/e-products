9c2f581e1a5d50f29bb9a02bedfea04a079d05a0
wptasmina <wptasmina@gmail.com>
2025-04-27 18:30:47 +0600

    add data fetch

--- src/Page/Product/All-Product/All_Product.jsx
+++ src/Page/Product/All-Product/All_Product.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import Product from './../Product';
+
+import { useEffect, useState } from 'react';
+import Card from '../../../components/product-card/card';
 
 export default function All_Product() {
+  const [products, setProducts] = useState()
+  const [loading, setLoading] = useState(true);
+
+  console.log(products);
+  
+
+  useEffect(() => {
+    fetch('/product.json')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+        setLoading(false);;
+      })
+      .catch(err => {
+        console.error('Error loading data:', err);
+      });
+  }, []);
+  
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center py-20 text-[#022dbb]">
+        <p className="text-sm">Loading all-product...</p>
+      </div>
+    );
+  }
+
   return (
     <div className='w-10/12 mx-auto'>
       <div>
         <h1>All Product Gallery</h1>
       </div>
       <div className=''>
-      <div>search filter</div>
-      <div className='gird grid-cols-1 sm:grid-cols-2 md:grid-cols-4'>
-      </div>
+        <div>search filter</div>
+        <div className='gird grid-cols-1 sm:grid-cols-2 md:grid-cols-4'>
+          {
+            products.map((item, index)=> (
+              <Card key={index.id} item={item} />
+            ))
+          }
+        </div>
       </div>
     </div>
   )
--- src/components/product-card/card.jsx
+++ src/components/product-card/card.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export default function card() {
+export default function Card({item}) {
+    console.log(item);
+    
   return (
     <div>card</div>
   )
